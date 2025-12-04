ccd2e530958326b3170a64652f09ddc3cc9731fb
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-12-05 01:32:57 +0200

    refactor: validate military time ranges with various spacing formats

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,22 +2,18 @@ import { MilitaryTimeValidator } from ".";
 
 describe("Military time validator", () => {
   describe("Time Range Format validation ('HH:MM - HH:MM')", () => {
-    it("knows that '01:12 - 14:32' is valid military time range format", () => {
-      const result = MilitaryTimeValidator.isValidRange();
-      expect(result.isValid).toBe(true);
-      expect(result.errors).toHaveLength(0);
-    });
-
-    it("knows that '01:12 -14:32' is valid military time range format", () => {
-      const result = MilitaryTimeValidator.isValidRange();
-      expect(result.isValid).toBe(true);
-      expect(result.errors).toHaveLength(0);
-    });
-
-    it("knows that '01:12- 14:32' is valid military time range format", () => {
-      const result = MilitaryTimeValidator.isValidRange();
-      expect(result.isValid).toBe(true);
-      expect(result.errors).toHaveLength(0);
+    describe("Valid time range format", () => {
+      it.each([
+        "01:12 - 14:32",
+        "01:12 -14:32",
+        "01:12- 14:32",
+        "00:00-23:59",
+        " 22:22 - 02:43 ",
+      ])("knows '%s' is a valid format", () => {
+        const result = MilitaryTimeValidator.isValidRange();
+        expect(result.isValid).toBe(true);
+        expect(result.errors).toHaveLength(0);
+      });
     });
   });
 });
