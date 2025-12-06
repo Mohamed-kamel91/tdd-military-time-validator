d296acbabc9c0d92ea8b84356fb034407f63a4ee
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-12-07 01:49:51 +0200

    refactor(test): improve readibility with parameteriazation and use better test assertions

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,4 +1,5 @@
 import { MilitaryTimeValidator } from ".";
+import { TIME_RANGE_ERRORS } from "./constants";
 
 describe("Military time validator", () => {
   describe("Time Range Format validation ('HH:MM - HH:MM')", () => {
@@ -9,7 +10,7 @@ describe("Military time validator", () => {
         "01:12- 14:32",
         "00:00-23:59",
         " 22:22 - 02:43 ",
-      ])("knows '%s' is a valid", (timeRange) => {
+      ])("knows '%s' is valid", (timeRange) => {
         const result = MilitaryTimeValidator.isValidRange(timeRange);
         expect(result.isValid).toBe(true);
         expect(result.errors).toHaveLength(0);
@@ -18,39 +19,25 @@ describe("Military time validator", () => {
 
     describe("Invalid time range formats", () => {
       describe("Time range must not be empty", () => {
-        it.each(["", "  "])("knows '%s' is not a valid", () => {
+        it.each(["", "  "])("returns error for empty time range: '%s'", () => {
           const result = MilitaryTimeValidator.isValidRange("");
-
           expect(result.isValid).toBe(false);
-          expect(result.errors).toHaveLength(1);
-          expect(result.errors[0]).toEqual({
-            type: "invalid_format",
-            message: "Time range cannot be empty",
-          });
+          expect(result.errors).toContainEqual(TIME_RANGE_ERRORS.EMPTY);
         });
       });
 
-      it("knows that time range missing dash (-) seperator such as '01:12 14:32' is invalid", () => {
-        const result = MilitaryTimeValidator.isValidRange("01:12 14:32");
-
-        expect(result.isValid).toBe(false);
-        expect(result.errors).toHaveLength(1);
-        expect(result.errors[0]).toEqual({
-          type: "invalid_format",
-          message: "Time range must contain a '-' separator",
-        });
+      describe("Time range must contain '-' seperator", () => {
+        it.each(["01:12", "01:12 14:32"])(
+          "returns error for time range '%s' when '-' separator is missing",
+          (timeRange) => {
+            const result = MilitaryTimeValidator.isValidRange(timeRange);
+            expect(result.isValid).toBe(false);
+            expect(result.errors).toContainEqual(
+              TIME_RANGE_ERRORS.MISSING_SEPARATOR
+            );
+          }
+        );
       });
-
-      it("knows that time range missing dash (-) seperator such as '01:12' is invalid", () => {
-        const result = MilitaryTimeValidator.isValidRange("01:12");
-
-        expect(result.isValid).toBe(false);
-        expect(result.errors).toHaveLength(1);
-        expect(result.errors[0]).toEqual({
-          type: "invalid_format",
-          message: "Time range must contain a '-' separator",
-        });
-      })
     });
   });
 });
